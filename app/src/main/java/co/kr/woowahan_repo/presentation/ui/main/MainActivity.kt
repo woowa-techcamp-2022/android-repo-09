22c5b6b68b2c0c02d355b70f70d526f7f03d37a1
hansh0101 <hansh0101@naver.com>
2022-07-12 15:42:12 +0900

    [RENAME] #1 - MainActivity 패키지 이동

--- app/src/main/java/co/kr/woowahan_repo/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/co/kr/woowahan_repo/presentation/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package co.kr.woowahan_repo
+package co.kr.woowahan_repo.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.kr.woowahan_repo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

59d01a4a96f9d2c8561faa31b735153b9ae59753
wjchoi96 <wjchoi96@naver.com>
2022-07-12 23:44:15 +0900

    [FEATURE] #2 Issues Adapter 생성. DiffUtil 사용해 List Update를 구현하기 위해 IssuesDiffUtilCallback 구현

---
+++ app/src/main/java/co/kr/woowahan_repo/presentation/ui/issues/IssuesAdapter.kt
@@ -0,0 +1,73 @@
+package co.kr.woowahan_repo.presentation.ui.issues
+
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import co.kr.woowahan_repo.databinding.ViewIssueItemBinding
+import co.kr.woowahan_repo.domain.GithubIssueModel
+import timber.log.Timber
+
+/**
+ * https://sohee1702.tistory.com/409
+ *
+ * 공강 : O(N)
+ * 시간 : O(N + D^2)
+ * => old/new 두 리스트의 합인 N개 / old가 new로 변환되기 위해 필요한 최소 작업갯수(==edit script) D
+ * 최대 사이즈는 2^26(67,108,864)개까지 지원
+ */
+class IssuesAdapter: RecyclerView.Adapter<IssuesAdapter.IssueItemViewHolder>() {
+    private val issues = ArrayList<GithubIssueModel>()
+    fun setList(issues: List<GithubIssueModel>){
+        this.issues.clear()
+        this.issues.addAll(issues)
+    }
+    fun updateList(issues: List<GithubIssueModel>){
+        val handler = Handler(Looper.getMainLooper())
+        val newList = ArrayList(issues)
+        Thread {
+            Timber.d("diff debug updateList thread start")
+            val diffCallback = IssuesDiffUtilCallback(this.issues, newList)
+            val diffRes = DiffUtil.calculateDiff(diffCallback)
+            handler.post {
+                this.setList(newList) // must call main thread
+                Timber.d("diff debug updateList post : setList[${this.issues.size}], newList[${newList.size}]")
+                diffRes.dispatchUpdatesTo(this)
+            }
+        }.start()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IssueItemViewHolder {
+        return IssueItemViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: IssueItemViewHolder, position: Int) {
+        holder.bind(issues[position])
+    }
+
+    override fun getItemCount(): Int {
+        return issues.size
+    }
+
+    class IssueItemViewHolder(
+        binding: ViewIssueItemBinding
+    ): RecyclerView.ViewHolder(binding.root){
+        companion object {
+            fun from(parent: ViewGroup): IssueItemViewHolder{
+                return IssueItemViewHolder(
+                    ViewIssueItemBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+        }
+
+        fun bind(item: GithubIssueModel){
+
+        }
+    }
+}

725fac2f24e529ad6f00de21a019df36d1c0c436
hansh0101 <hansh0101@naver.com>
2022-07-12 16:50:25 +0900

    [FEATURE] #1 - Github OAuth Access Token 발급 구현
    - BaseActivity 상속을 통한 DataBinding 적용

--- app/src/main/java/co/kr/woowahan_repo/presentation/ui/signin/SignInActivity.kt
+++ app/src/main/java/co/kr/woowahan_repo/presentation/ui/signin/SignInActivity.kt
@@ -1,12 +1,42 @@
 package co.kr.woowahan_repo.presentation.ui.signin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import co.kr.woowahan_repo.BuildConfig
 import co.kr.woowahan_repo.R
+import co.kr.woowahan_repo.databinding.ActivitySignInBinding
+import co.kr.woowahan_repo.presentation.ui.base.BaseActivity
+import co.kr.woowahan_repo.presentation.viewmodel.SignInViewModel
+import timber.log.Timber
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity<ActivitySignInBinding>() {
+    override val layoutResId: Int
+        get() = R.layout.activity_sign_in
+    private val signInViewModel by viewModels<SignInViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        binding.btnSignIn.setOnClickListener {
+            val url = "https://github.com/login/oauth/authorize" +
+                    "?client_id=${BuildConfig.GITHUB_CLIENT_ID}"
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Toast.makeText(applicationContext, "onNewIntent", Toast.LENGTH_LONG).show()
+        Timber.d("onNewIntent called => ${intent?.dataString}")
+        intent?.dataString?.let {
+            val start = it.indexOf("code")
+            Timber.d("start => $start")
+            val code = it.substring(start+5, it.length)
+            Timber.d("code => $code")
+            signInViewModel.getGithubOAuthAccessToken(code)
+        }
     }
 }

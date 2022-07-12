75bd3ab0668be5a48e5b7f7d82f23ef37685378f
hansh0101 <hansh0101@naver.com>
2022-07-12 15:36:42 +0900

    [FEATURE] #1 - SignInActivity 생성

---
+++ app/src/main/java/co/kr/woowahan_repo/presentation/ui/signin/SignInActivity.kt
@@ -0,0 +1,12 @@
+package co.kr.woowahan_repo.presentation.ui.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.kr.woowahan_repo.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}

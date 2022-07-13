1264309e3849257d5bae13ac5987624997a45187
wjchoi96 <wjchoi96@naver.com>
2022-07-13 23:02:33 +0900

    [FIX] #11 코루틴 Scope 로 호출 될 가능성이 있는 Api Service를 suspend fun 으로 설정
     - 안하면 에러발생
     - 수명주기가 끝나면 진행 중 코루틴도 정지해야 하는데, suspend 로 설정된 함수만이 정지할 수 있는 함수 라고 이해하고 있는데 정확하게 알고 있는 상태가 아니다.

--- app/src/main/java/co/kr/woowahan_repo/data/service/GithubIssuesService.kt
+++ app/src/main/java/co/kr/woowahan_repo/data/service/GithubIssuesService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface GithubIssuesService {
     @GET("issues")
-    fun fetchIssues(
+    suspend fun fetchIssues(
         @Query("state")state: String,
         @Query("page")page: Int = 1, // default is 1
         @Query("per_page")pageItemCount: Int = 100 // max is 100, default is 30

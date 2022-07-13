e62a62ad9515e2ac4de733c1ecb56ac59f23e955
wjchoi96 <wjchoi96@naver.com>
2022-07-13 23:16:54 +0900

    [FEATURE] #11 Add SingleEvent
     - LiveDate Event 의 중복 실행 방지를 위해 Single Event 를 생성하였고, Main 화면에 적용

--- app/src/main/java/co/kr/woowahan_repo/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/co/kr/woowahan_repo/presentation/ui/main/MainActivity.kt
@@ -1,10 +1,13 @@
 package co.kr.woowahan_repo.presentation.ui.main
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.activity.viewModels
+import androidx.fragment.app.commit
 import co.kr.woowahan_repo.R
 import co.kr.woowahan_repo.databinding.ActivityMainBinding
 import co.kr.woowahan_repo.presentation.ui.base.BaseActivity
+import co.kr.woowahan_repo.presentation.ui.issues.IssuesFragment
 import co.kr.woowahan_repo.presentation.viewmodel.MainViewModel
 import timber.log.Timber
 
@@ -45,31 +48,39 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             binding.btnTab2.isSelected = it
         }
 
-        viewModel.showTabOneEvent.observe(this) {
-            Timber.d("show tab 1")
-//            supportFragmentManager.commit {
-//                replace(
-//                    R.id.container_fragment_main,
-//                    Fragment()
-//                )
-//            }
+        viewModel.showTabOneEvent.observe(this) { event ->
+            event.getContentIfNotHandled()?.let {
+                Timber.d("show tab 1")
+                supportFragmentManager.commit {
+                    replace(
+                        R.id.container_fragment_main,
+                        IssuesFragment.newInstance()
+                    )
+                }
+            }
         }
-        viewModel.showTabTwoEvent.observe(this) {
-            Timber.d("show tab 2")
-//            supportFragmentManager.commit {
-//                replace(
-//                    R.id.container_fragment_main,
-//                    Fragment()
-//                )
-//            }
+        viewModel.showTabTwoEvent.observe(this) {  event ->
+            event.getContentIfNotHandled()?.let {
+                Timber.d("show tab 2")
+                supportFragmentManager.commit {
+                    replace(
+                        R.id.container_fragment_main,
+                        NotificationsFragment.newInstance()
+                    )
+                }
+            }
         }
 
-        viewModel.showSearchEvent.observe(this){
-            Timber.d("show search view")
+        viewModel.showSearchEvent.observe(this){ event ->
+            event.getContentIfNotHandled()?.let {
+                Timber.d("show search view")
+            }
         }
 
-        viewModel.showProfileEvent.observe(this){
-            Timber.d("show profile view")
+        viewModel.showProfileEvent.observe(this){ event ->
+            event.getContentIfNotHandled()?.let {
+                Timber.d("show profile view")
+            }
         }
     }
 }
--- app/src/main/java/co/kr/woowahan_repo/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/co/kr/woowahan_repo/presentation/viewmodel/MainViewModel.kt
@@ -3,6 +3,7 @@ package co.kr.woowahan_repo.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import co.kr.woowahan_repo.data.SingleEvent
 
 class MainViewModel: ViewModel() {
     private val _tabOneSelected = MutableLiveData<Boolean>()
@@ -11,37 +12,37 @@ class MainViewModel: ViewModel() {
     private val _tabTwoSelected = MutableLiveData<Boolean>()
     val tabTwoSelected: LiveData<Boolean> = _tabTwoSelected
 
-    private val _showTabOneEvent = MutableLiveData<Unit>()
-    val showTabOneEvent: LiveData<Unit> = _showTabOneEvent
+    private val _showTabOneEvent = MutableLiveData<SingleEvent<Unit>>()
+    val showTabOneEvent: LiveData<SingleEvent<Unit>> = _showTabOneEvent
 
-    private val _showTabTwoEvent = MutableLiveData<Unit>()
-    val showTabTwoEvent: LiveData<Unit> = _showTabTwoEvent
+    private val _showTabTwoEvent = MutableLiveData<SingleEvent<Unit>>()
+    val showTabTwoEvent: LiveData<SingleEvent<Unit>> = _showTabTwoEvent
 
-    private val _showSearchEvent = MutableLiveData<Unit>()
-    val showSearchEvent: LiveData<Unit> = _showSearchEvent
+    private val _showSearchEvent = MutableLiveData<SingleEvent<Unit>>()
+    val showSearchEvent: LiveData<SingleEvent<Unit>> = _showSearchEvent
 
-    private val _showProfileEvent = MutableLiveData<Unit>()
-    val showProfileEvent: LiveData<Unit> = _showProfileEvent
+    private val _showProfileEvent = MutableLiveData<SingleEvent<Unit>>()
+    val showProfileEvent: LiveData<SingleEvent<Unit>> = _showProfileEvent
 
 
     fun clickTabOne(){
         _tabOneSelected.value = true
         _tabTwoSelected.value = false
-        _showTabOneEvent.value = Unit
+        _showTabOneEvent.value = SingleEvent(Unit)
     }
 
     fun clickTabTwo(){
         _tabOneSelected.value = false
         _tabTwoSelected.value = true
-        _showTabTwoEvent.value = Unit
+        _showTabTwoEvent.value = SingleEvent(Unit)
     }
 
     fun clickSearch(){
-        _showSearchEvent.value = Unit
+        _showSearchEvent.value = SingleEvent(Unit)
     }
 
     fun clickProfile(){
-        _showProfileEvent.value = Unit
+        _showProfileEvent.value = SingleEvent(Unit)
     }
 
 }

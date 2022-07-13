0a5bc30bf0f6a99814e24624d691e29b9586be2f
wjchoi96 <wjchoi96@naver.com>
2022-07-13 16:21:56 +0900

    [REFACTOR] #2 테스트 코드를 위해 Github Issue Model 의 dummy data 생성 메소드 추가

--- app/src/main/java/co/kr/woowahan_repo/domain/GithubIssueModel.kt
+++ app/src/main/java/co/kr/woowahan_repo/domain/GithubIssueModel.kt
@@ -9,7 +9,16 @@ data class GithubIssueModel(
     val lastUpdateDate: String // 2011-01-26T19:14:43Z, "yyyy-MM-dd'T'HH:mm:ss'Z'"
 ) {
     companion object {
-        fun getDummyDate(): String{
+        fun getDummy(state: IssueState, seed: Int? = null): GithubIssueModel{
+            val seedValue = seed ?: Random.nextInt(100)
+            return GithubIssueModel(
+                "Repository Name[$seedValue]",
+                "Issue Title[$seedValue]",
+                if(state == IssueState.All) IssueState.values()[Random.nextInt(2)] else state,
+                getDummyDate()
+            )
+        }
+        private fun getDummyDate(): String{
             val example1 = "2011-01-26T19:14:43Z"
             val example2 = "2022-01-26T19:14:43Z"
             val example3 = "2022-06-26T19:14:43Z"

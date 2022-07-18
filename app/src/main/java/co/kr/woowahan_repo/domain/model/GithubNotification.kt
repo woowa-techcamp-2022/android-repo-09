cf96f1afbbc93ec3bda827b2af4f5cffa92d7117
hansh0101 <hansh0101@naver.com>
2022-07-18 16:08:24 +0900

    [REFACTOR] #17 - updatedAt var -> val 수정

--- app/src/main/java/co/kr/woowahan_repo/domain/model/GithubNotification.kt
+++ app/src/main/java/co/kr/woowahan_repo/domain/model/GithubNotification.kt
@@ -4,7 +4,7 @@ data class GithubNotification(
     val id: String,
     val repositoryName: String,
     val number: String,
-    var updatedAt: String,
+    val updatedAt: String,
     val repositoryImage: String,
     val title: String,
     val comments: String

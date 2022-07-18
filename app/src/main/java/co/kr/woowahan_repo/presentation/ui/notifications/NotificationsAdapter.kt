ad83e117bdf500e33cf920515c1cdf24e4eb3f00
hansh0101 <hansh0101@naver.com>
2022-07-18 13:52:23 +0900

    [FEATURE] #17 - Notification RecyclerView Adapter 구현

---
+++ app/src/main/java/co/kr/woowahan_repo/presentation/ui/notifications/NotificationsAdapter.kt
@@ -0,0 +1,70 @@
+package co.kr.woowahan_repo.presentation.ui.notifications
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import co.kr.woowahan_repo.R
+import co.kr.woowahan_repo.databinding.ViewNotificationItemBinding
+import co.kr.woowahan_repo.domain.model.GithubNotification
+import co.kr.woowahan_repo.util.DateUtil
+import coil.load
+import coil.transform.CircleCropTransformation
+
+class NotificationsAdapter : RecyclerView.Adapter<NotificationsAdapter.NotificationViewHolder>() {
+    private var itemList = mutableListOf<GithubNotification>()
+
+    class NotificationViewHolder(private val binding: ViewNotificationItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: GithubNotification) {
+            data.updatedAt = DateUtil.getLastUpdateIntervalDateString(data.updatedAt)
+            binding.notification = data
+            binding.ivRepositoryImage.load(data.repositoryImage) {
+                transformations(CircleCropTransformation())
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
+        val binding = DataBindingUtil.inflate<ViewNotificationItemBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.view_notification_item,
+            parent,
+            false
+        )
+        return NotificationViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
+    override fun getItemCount(): Int = itemList.size
+
+    fun updateList(newItemList: List<GithubNotification>) {
+        val diffCallback = DiffUtilCallback(itemList, newItemList)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+        itemList = newItemList.toMutableList()
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    companion object {
+        class DiffUtilCallback(
+            private val oldItems: List<GithubNotification>,
+            private val newItems: List<GithubNotification>
+        ) : DiffUtil.Callback() {
+            override fun getOldListSize(): Int = oldItems.size
+
+            override fun getNewListSize(): Int = newItems.size
+
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                oldItems[oldItemPosition].id == newItems[newItemPosition].id
+
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                oldItems[oldItemPosition] == newItems[newItemPosition]
+
+        }
+    }
+}

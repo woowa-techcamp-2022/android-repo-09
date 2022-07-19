d53d26f6afafe9a3e9d73586874d63afa36eefb1
hansh0101 <hansh0101@naver.com>
2022-07-20 03:22:38 +0900

    [FEATURE] #37 - Notification 읽기 API 연결

--- app/src/main/java/co/kr/woowahan_repo/data/service/GithubNotificationsService.kt
+++ app/src/main/java/co/kr/woowahan_repo/data/service/GithubNotificationsService.kt
@@ -2,6 +2,8 @@ package co.kr.woowahan_repo.data.service
 
 import co.kr.woowahan_repo.data.model.response.GithubNotificationResponse
 import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubNotificationsService {
@@ -10,4 +12,9 @@ interface GithubNotificationsService {
         @Query("page") page: Int,
         @Query("per_page") perPage: Int
     ): List<GithubNotificationResponse>
+
+    @PATCH("notifications/threads/{thread_id}")
+    suspend fun patchNotificationAsRead(
+        @Path("thread_id") threadId: String
+    )
 }

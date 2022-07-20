772f2173559287cebc5455487921200517fa481c
hansh0101 <hansh0101@naver.com>
2022-07-20 11:28:36 +0900

    [FEATURE] #37 - Notification RecyclerView 스와이프 되었을 때 Layout 표시되도록 구현

--- app/src/main/java/co/kr/woowahan_repo/util/NotificationTouchHelper.kt
+++ app/src/main/java/co/kr/woowahan_repo/util/NotificationTouchHelper.kt
@@ -1,12 +1,14 @@
 package co.kr.woowahan_repo.util
 
+import android.graphics.Canvas
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import co.kr.woowahan_repo.R
 import co.kr.woowahan_repo.presentation.ui.notifications.NotificationsAdapter
 
 class NotificationTouchHelper(
-    private val notificationsAdapter : NotificationsAdapter
-): ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+    private val notificationsAdapter: NotificationsAdapter
+) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
     override fun onMove(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder,
@@ -17,5 +19,38 @@ class NotificationTouchHelper(
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
         notificationsAdapter.removeItem(viewHolder.adapterPosition)
+    }
+
+    override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+        getDefaultUIUtil().clearView(viewHolder.itemView.findViewById(R.id.layout_notification))
+    }
+
+    override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+        viewHolder?.let {
+            getDefaultUIUtil().onSelected(viewHolder.itemView.findViewById(R.id.layout_notification))
+        }
+    }
+
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            getDefaultUIUtil().onDraw(
+                c,
+                recyclerView,
+                viewHolder.itemView.findViewById(R.id.layout_notification),
+                dX,
+                dY,
+                actionState,
+                isCurrentlyActive
+            )
+        }
     }
 }

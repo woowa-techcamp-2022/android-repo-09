52fb8502ce2112b7852041064970754875201a6b
wjchoi96 <wjchoi96@naver.com>
2022-07-21 22:01:02 +0900

    [FREATURE] #53 프로필 화면에 로딩효과, 뒤로가기 버튼 추가

--- app/src/main/java/co/kr/woowahan_repo/presentation/ui/profile/ProfileActivity.kt
+++ app/src/main/java/co/kr/woowahan_repo/presentation/ui/profile/ProfileActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.graphics.Paint
 import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import co.kr.woowahan_repo.R
 import co.kr.woowahan_repo.databinding.ActivityProfileBinding
 import co.kr.woowahan_repo.presentation.ui.base.BaseActivity
@@ -30,6 +32,12 @@ class ProfileActivity : BaseActivity<ActivityProfileBinding>() {
     }
 
     private fun initView() {
+        setSupportActionBar(binding.toolBar)
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(false)
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_selector_arrow_left) // 뒤로가기 버튼 이미지 설정
+        }
         binding.tvLink.paintFlags = Paint.UNDERLINE_TEXT_FLAG
         binding.tvMail.paintFlags = Paint.UNDERLINE_TEXT_FLAG
     }
@@ -51,14 +59,26 @@ class ProfileActivity : BaseActivity<ActivityProfileBinding>() {
     }
 
     private fun observeData() {
+        viewModel.dataLoading.observe(this) {
+            binding.progress.isVisible = it
+        }
+
         viewModel.profile.observe(this) {
             binding.ivProfile.load(it.profileImage) {
                 transformations(CircleCropTransformation())
             }
         }
+
         viewModel.finishEvent.observe(this) {
             showToast(it)
             finish()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

f3d07fe6a56a825b0edaa8fc3206056d9cdb9542
wjchoi96 <wjchoi96@naver.com>
2022-07-23 23:23:39 +0900

    [FIX] Profile Data Source 구현체 변경
     - 매번 새로운 로그인을 진행하는데, 구현체를 Shared Preferences 로 사용한다면 의도치 않은 문제가 발생할 수 있다. 때문에 그 구현체를 변경

--- app/src/main/java/co/kr/woowahan_repo/di/DataSourceModule.kt
+++ app/src/main/java/co/kr/woowahan_repo/di/DataSourceModule.kt
@@ -1,21 +1,26 @@
 package co.kr.woowahan_repo.di
 
+import co.kr.woowahan_repo.data.datasource.GithubProfileDataSourceImpl
 import co.kr.woowahan_repo.data.datasource.WoowahanSharedPreferences
 import co.kr.woowahan_repo.domain.datasource.GithubProfileDataSource
 import co.kr.woowahan_repo.domain.datasource.GithubTokenDataSource
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataSourceModule {
 
     @Binds
+    @Singleton
     abstract fun bindGithubTokenDataSource(impl: WoowahanSharedPreferences): GithubTokenDataSource
-    
+
     @Binds
-    abstract fun bindGithubProfileSource(impl: WoowahanSharedPreferences): GithubProfileDataSource
+    @Singleton
+    abstract fun bindGithubProfileDataSource(impl: GithubProfileDataSourceImpl): GithubProfileDataSource
 
 }
